9d8397dd29acd0d58b0d6219b82f8ccede30601c
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2020-12-22 16:21:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,3 @@
 echo "Instalando SimpleScreenRecorder, Pavucontrol, Pulseaudio y PiKISS. CTRL+C para cancelar"
 sudo apt-get install simplescreenrecorder pavucontrol
 curl -sSL https://git.io/JfAPE | bash
-echo "Ea, listo. Ahora toca magic-air-copy-pikiss.txt"
-
